a96e313770a55fcb40b7c92ac528474709e6e051
Sushant Sharma <sushant.wurk@gmail.com>
2023-10-19 23:50:42 +0530

    Footer comp

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Container } from "react-bootstrap";
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <h1>Shop</h1>
         </Container>
       </main>
+      <Footer />
     </Fragment>
   );
 }
---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            <p>Shop cc 2023</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
