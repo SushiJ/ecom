c62a2a4902eb7bd50d655297e0708cbe770c9b6f
Sushant Sharma <sushant.wurk@gmail.com>
2023-10-23 15:33:26 +0530

    some smol custom css

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,6 +7,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import "./assets/bootstrap.css";
 import "./assets/index.css";
 import App from "./App.tsx";
 
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -23,7 +23,7 @@ function Product(props: { product: (typeof products)[0] }) {
   return (
     <Card className="my-3 p-3 rounded h-100">
       <Link to={`/products/${props.product._id}`}>
-        <Card.Img src={props.product.image} variant="top" />
+        <Card.Img src={props.product.image} variant="top" className="rounded" />
       </Link>
       <Card.Body>
         <Link to={`/product/${props.product._id}`}>
@@ -58,11 +58,11 @@ function RenderRatingIcon(props: { value: number }) {
       <span>
         {[1, 2, 3, 4, 5].map((num) =>
           props.value >= num ? (
-            <Icon icon="fluent:star-12-filled" />
+            <Icon icon="fluent:star-12-filled" width="20" height="20" />
           ) : props.value >= num - 0.5 ? (
-            <Icon icon="fluent:star-half-12-filled" />
+            <Icon icon="fluent:star-half-12-filled" width="20" height="20" />
           ) : (
-            <Icon icon="fluent:star-12-regular" />
+            <Icon icon="fluent:star-12-regular" width="20" height="20" />
           )
         )}
       </span>
