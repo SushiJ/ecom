1325c06dae3dd973b0526c29de4f9fa51e5f43e6
Sushant Sharma <sushant.wurk@gmail.com>
2023-10-28 21:30:05 +0530

    Server app.ts

--- sv/src/index.ts
+++ sv/src/index.ts
@@ -1,1 +1,19 @@
-console.log("testing");
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+config();
+
+const PORT = process.env.PORT ?? 42069;
+const app = express();
+
+app.use(morgan("common"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.get("/check", (_req, res) => res.sendStatus(200));
+
+app.listen(PORT, () => console.log("YEP"));
