7f20c3f7c617ef643eab0c16663c094bdf47cc89
Sushant Sharma <sushant.wurk@gmail.com>
2023-11-18 21:52:52 +0530

    Redux init

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,11 +6,13 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./assets/bootstrap.css";
 import "./assets/index.css";
 import App from "./App.tsx";
 
+import { store } from "./store";
 import { Home } from "./pages/Home.tsx";
 import { Product } from "./pages/Product.tsx";
 
@@ -19,12 +21,14 @@ const routes = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
       <Route path="/products/:id" element={<Product />} />
-    </Route>
-  )
+    </Route>,
+  ),
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </React.StrictMode>,
 );
