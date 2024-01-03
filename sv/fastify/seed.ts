e848dbbe24995bd3b6d1d745839e5c2ef4a25689
Sushant Sharma <sushant.wurk@gmail.com>
2024-01-04 00:24:27 +0530

    Chore: improve db seeding

--- sv/fastify/seed.ts
+++ sv/fastify/seed.ts
@@ -1,17 +1,12 @@
-import { productModel } from "./src/models/Product";
-import connect from "./src/utils/connection";
-import { products } from "./initialData";
+import { products, users } from "./initialData.js";
+import { MongoClient } from "mongodb";
 
 type Product = Omit<(typeof products)[0], "_id">;
 
-async function saveProduct(product: Product) {
-  productModel
-    .insertMany(product)
-    .then(() => console.log("Document saved successfully"))
-    .catch((err) => console.error("Error saving document:", err));
-}
+type User = (typeof users)[0];
 
-async function populateProduct() {
+async function _seedProducts() {
+  const productsArr: Array<Product> = [];
   for (const productData of products) {
     const product: Product = {
       name: productData["name"],
@@ -24,17 +19,41 @@ async function populateProduct() {
       countInStock: productData["countInStock"],
       numReviews: productData["numReviews"],
     };
-    await saveProduct(product);
+    productsArr.push(product);
   }
 }
 
-function main() {
-  connect()
-    .then(() => {
-      console.log("CONNECTED");
-      populateProduct().catch((e) => console.error(e));
-    })
-    .catch((e) => console.error(e));
+const uri = "mongodb://mongo:mongo@localhost:27017/";
+const client = new MongoClient(uri);
+
+async function seedDB() {
+  // Connection URL
+  await client.connect().catch((e) => console.error(e));
+  console.log("Connected correctly to server");
+
+  const collection = client.db("ecom").collection("users");
+
+  // The drop() command destroys all data from a collection.
+  // Make sure you run it against proper database and collection.
+  collection.drop();
+
+  const userArr: Array<User> = [];
+
+  for (const u of users) {
+    const user: User = {
+      name: u["name"],
+      password: u["password"],
+      email: u["email"],
+      isAdmin: u["isAdmin"],
+    };
+    userArr.push(user);
+  }
+  await collection.insertMany(userArr).catch((e) => console.error(e));
+  await collection.findOne().then(console.log);
+  return "Database seeded";
 }
 
-main();
+seedDB()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
