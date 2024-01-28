1eaa0bdea179f05a14c2b7e0bf704a6013a6a996
Sushant Sharma <sushant.wurk@gmail.com>
2024-01-28 20:52:07 +0530

    web: better login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -8,6 +8,7 @@ import { useAppSelector } from "../hooks/redux";
 import { useLoginMutation } from "../features/user/slice";
 import { setCredentials } from "../features/auth/slice";
 import { toast } from "react-toastify";
+import Loader from "../components/Loader";
 
 const FormContainer = ({ children }: { children: JSX.Element }) => {
   return (
@@ -22,50 +23,6 @@ const FormContainer = ({ children }: { children: JSX.Element }) => {
 };
 
 const Login = () => {
-  const initialState = {
-    email: "",
-    password: "",
-  };
-
-  // @typescript-eslint/no-unused-vars
-  // eslint-disable-next-line
-  const [initialValues, _] = useState(initialState);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    mode: "onTouched",
-    reValidateMode: "onSubmit",
-    // reValidateMode: "onChange",
-    defaultValues: initialValues,
-  });
-
-  const onSubmit = async (values: typeof initialState) => {
-    console.log("Values:::", values);
-    console.log("Values:::", JSON.stringify(values));
-    try {
-      const res = await login({
-        email: values.email,
-        password: values.password,
-      }).unwrap();
-      dispatch(setCredentials(res));
-      navigate(redirect);
-      toast.success("Logged in successfully");
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      console.log("ERROR:::", error);
-      toast(error);
-    }
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onError = (error: any) => {
-    console.log("ERROR:::", error);
-    toast.error(error?.data?.message || error.error);
-  };
-
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
@@ -77,59 +34,102 @@ const Login = () => {
 
   const sp = new URLSearchParams(search);
   const redirect = sp.get("redirect") || "/";
-  useEffect(() => {}, []);
 
-  // useEffect(() => {
-  //   if (userInfo) {
-  //     navigate(redirect);
-  //   }
-  // }, [navigate, redirect, userInfo]);
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [initialValues, _] = useState(initialState);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setFocus,
+  } = useForm({
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+    defaultValues: initialValues,
+  });
+
+  useEffect(() => {
+    setFocus("email");
+  }, [setFocus]);
+
+  const onSubmit = async (values: typeof initialState) => {
+    try {
+      const res = await login({
+        email: values.email,
+        password: values.password,
+      }).unwrap();
+      dispatch(setCredentials(res));
+      navigate(redirect);
+      toast.success("Logged in successfully");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log("ERROR:::", error);
+      toast(error.data.error, {
+        type: "error",
+      });
+    }
+  };
 
   return (
     <FormContainer>
       <Fragment>
         <h1>Login</h1>
-        <Form onSubmit={handleSubmit(onSubmit, onError)}>
-          <Form.Group className="my-2" controlId="email">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              {...register("email", { required: "Email is required" })}
-            ></Form.Control>
-            {errors.email && (
-              <Form.Text className="text-danger">
-                {errors.email.message}
-              </Form.Text>
-            )}
-          </Form.Group>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className={isLoading ? "opacity-50" : "opacity-100"}>
+            <Form.Group className="my-2" controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                {...register("email", { required: "Email is required" })}
+              ></Form.Control>
+              {errors.email && (
+                <Form.Text className="text-danger">
+                  {errors.email.message}
+                </Form.Text>
+              )}
+            </Form.Group>
 
-          <Form.Group className="my-2" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              {...register("password", { required: "password is required" })}
-            ></Form.Control>
-            {errors.password && (
-              <Form.Text className="text-danger">
-                {errors.password.message}
-              </Form.Text>
-            )}
-          </Form.Group>
+            <Form.Group className="my-2" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                {...register("password", { required: "password is required" })}
+              ></Form.Control>
+              {errors.password && (
+                <Form.Text className="text-danger">
+                  {errors.password.message}
+                </Form.Text>
+              )}
+            </Form.Group>
 
-          <Button disabled={isLoading} type="submit" variant="primary">
-            Sign In
-          </Button>
-          {/* {isLoading && <Loader />} */}
+            <Button disabled={isLoading} type="submit" variant="primary">
+              Sign In
+            </Button>
+          </fieldset>
+          {isLoading && <Loader />}
         </Form>
         <Row className="py-3">
           <Col>
-            New Customer?{" "}
+            New Customer?
             <Link
               to={redirect ? `/register?redirect=${redirect}` : "/register"}
+              className="ms-1"
             >
-              Register
+              Sign up
             </Link>
           </Col>
         </Row>
