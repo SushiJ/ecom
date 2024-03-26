4c58c9267108c6eaa9d7a7f5ef9411417af49108
Sushant Sharma <sushant.wurk@gmail.com>
2024-03-26 21:11:18 +0530

    Fix: typo

--- web/src/components/ProductCarousel.tsx
+++ web/src/components/ProductCarousel.tsx
@@ -27,7 +27,7 @@ function ProductCarousel() {
     <Carousel pause="hover" className="bg-primary mb-4">
       {products!.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
+          <Link to={`/products/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className="carousel-caption">
               <h2 className="text-white text-right">
