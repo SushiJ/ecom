154e31c6f710a4bf7e9f2c3719d2486568654d70
Sushant Sharma <sushant.wurk@gmail.com>
2024-12-10 22:47:06 +0530

    Chore. mig. cart page (WIP)

--- web/src/pages/Cart.tsx
+++ web/src/pages/Cart.tsx
@@ -1,108 +1,149 @@
-import {
-  Col,
-  ListGroup,
-  Row,
-  Image,
-  Form,
-  Button,
-  Card,
-} from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Icon } from "@iconify/react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { removeFromCart } from "../features/cart/slice";
 
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import truncate from "@/lib/truncate";
+
 export default function CartPage() {
   const { products, totalAmount } = useAppSelector((state) => state.cart);
   const dispatch = useAppDispatch();
+
   if (products.length === 0) {
     return (
       <div>
-        <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
-        <p>
-          Your cart is empty <Link to="/">Go back</Link>
+        <h1 className="text-neutral-700 mb-2">Shopping Cart</h1>
+        <p className="italic">
+          Your cart is empty{" "}
+          <Link to="/" className="underline">
+            Go back
+          </Link>
         </p>
       </div>
     );
   }
 
   return (
-    <Row>
-      <Col md="8">
-        <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
-        <Link className="my-3 btn btn-outline-primary" to="/">
+    <>
+      <div>
+        <Link
+          className="text-xs italic bg-neutral-200 px-2 py-1 rounded shadow-md"
+          to="/"
+        >
           Go Back
         </Link>
-        <ListGroup variant="flush">
-          {products.map(({ product, quantity }) => {
-            return (
-              <ListGroup.Item key={product._id}>
-                <Row>
-                  <Col md="2">
-                    <Image
-                      src={product.image}
-                      alt={product.name}
-                      fluid
-                      rounded
-                    />
-                  </Col>
-                  <Col md="3">
-                    <Link to={`/products/${product._id}`}>{product.name}</Link>
-                  </Col>
-                  <Col md="2">
+        <p className="text-center italic text-neutral-700 mb-8 mt-4">
+          Shopping cart
+        </p>
+        <div className="space-y-8">
+          {products.map(({ product, quantity }) => (
+            <div className="grid md:grid-cols-2 lg:grid-cols-3">
+              <div className="lg:col-span-1 object-contain md:mr-4 grid place-items-center mx-auto">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="xs:max-h-60 xs:max-w-60 mx-auto rounded my-2 md:my-0"
+                />
+              </div>
+              <div className="lg:col-span-2 relative">
+                <Card key={product._id} className="p-4">
+                  <Link to={`/products/${product._id}`}>{product.name}</Link>
+                  <div className="space-y-2">
                     <p>${product.price}</p>
-                  </Col>
-                  <Col md="2">
-                    <Form.Control
-                      as="select"
-                      value={quantity}
-                      onChange={() => {}}
-                    >
-                      {Array.from(Array(product.countInStock).keys(), (v) => {
-                        return (
-                          <option key={v + 1} value={v + 1}>
-                            {v + 1}
-                          </option>
-                        );
-                      })}
-                    </Form.Control>
-                  </Col>
-                  <Col md="2">
+                    <p className="text-xs">{product.description}</p>
+                  </div>
+                  <div className="flex justify-between items-center mt-10">
+                    <p className="text-sm">Quantity: {quantity}</p>
                     <Button
                       type="button"
-                      variant="light"
-                      onClick={() => dispatch(removeFromCart(product))}
+                      variant="destructive"
+                      // className="mt-2 absolute bottom-0 right-0"
+                      onClick={() => dispatch(removeFromCart(product._id))}
                     >
-                      <Icon
-                        icon="fluent:delete-16-regular"
-                        style={{ color: "#d9534f" }}
-                        width="24"
-                        height="24"
-                      />
+                      Remove item
                     </Button>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            );
-          })}
-        </ListGroup>
-      </Col>
-      <Col md="4">
-        <Card>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h2>Subtotal ({products.length} items):</h2>$
-              {totalAmount.toFixed(2)}
-            </ListGroup.Item>
-            <ListGroup.Item disabled={products.length === 0}>
-              <Link to="/shipping" className="btn btn-outline-primary">
-                Proceed to Checkout
-              </Link>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
-      </Col>
-    </Row>
+                  </div>
+                </Card>
+              </div>
+              {/* TODO: add form here for updating quantity later */}
+
+              {/*   <Form {...form}> */}
+              {/*     <form */}
+              {/*       onSubmit={form.handleSubmit(onSubmit)} */}
+              {/*       className="flex flex-col space-y-8" */}
+              {/*     > */}
+              {/*       <FormField */}
+              {/*         control={form.control} */}
+              {/*         name="qty" */}
+              {/*         render={({ field }) => ( */}
+              {/*           <FormItem className="flex space-y-0 items-center space-x-2 mb-5"> */}
+              {/*             <FormLabel>Quantity:</FormLabel> */}
+              {/*             <FormControl> */}
+              {/*               <Select */}
+              {/*                 onValueChange={field.onChange} */}
+              {/*                 defaultValue={quantity.toString()} */}
+              {/*               > */}
+              {/*                 <FormControl> */}
+              {/*                   <SelectTrigger> */}
+              {/*                     <SelectValue placeholder="Select quantity" /> */}
+              {/*                   </SelectTrigger> */}
+              {/*                 </FormControl> */}
+              {/*                 <SelectContent> */}
+              {/*                   {Array.from( */}
+              {/*                     Array(product.countInStock).keys(), */}
+              {/*                     (v) => { */}
+              {/*                       return ( */}
+              {/*                         <SelectItem value={`${v + 1}`} key={v}> */}
+              {/*                           {v + 1} */}
+              {/*                         </SelectItem> */}
+              {/*                       ); */}
+              {/*                     }, */}
+              {/*                   )} */}
+              {/*                 </SelectContent> */}
+              {/*               </Select> */}
+              {/*             </FormControl> */}
+              {/*             <FormMessage /> */}
+              {/*           </FormItem> */}
+              {/*         )} */}
+              {/*       /> */}
+              {/*     </form> */}
+              {/*   </Form> */}
+              {/* </div> */}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col mt-8">
+        <div className="text-right">
+          <p>Subtotal ({products.length} items):</p>
+          <p>$ {totalAmount?.toFixed(2)}</p>
+        </div>
+        <Button className="mt-4">
+          <Link to="/shipping" className="">
+            Proceed to Checkout
+          </Link>
+        </Button>
+      </div>
+    </>
   );
 }
