e45284c3001a37315b8d77cfbe2dcc441e103030
Sushant Sharma <sushant.wurk@gmail.com>
2024-12-10 21:57:22 +0530

    Fix. bug with totalAmount

--- web/src/features/cart/slice.ts
+++ web/src/features/cart/slice.ts
@@ -3,12 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import { getLocalStorage } from "../../lib/localStorage";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../store";
-import { Product } from "../../types";
+import { Product } from "../../types/product";
 
 type AddToCart = {
   data: Product;
   quantity: number;
 };
+
+type TUpdateQuantityOfItem = AddToCart;
 
 type ShippingAddress = {
   address: string;
@@ -71,17 +73,44 @@ const cartSlice = createSlice({
       localStorage.setItem("cart", JSON.stringify(state));
     },
     // TODO: Ability to remove products 1 by 1;
-    removeFromCart: (state, action: PayloadAction<Product>) => {
-      const product = action.payload;
+    // TODO: Round out the numbers
+    removeFromCart: (state, action: PayloadAction<Product["_id"]>) => {
+      const productId = action.payload;
+
       let productQuantity: number | null = null;
+
+      const productToRemove = state.products.find(
+        (p) => p.product._id === productId,
+      );
+      if (!productToRemove) return;
+
       state.products = state.products.filter((p) => {
-        productQuantity = p.quantity;
-        return p.product._id !== product._id;
+        productQuantity = productToRemove.quantity;
+        return p.product._id !== productId;
       });
-      if (productQuantity) {
-        state.totalAmount -= product.price * productQuantity;
-        localStorage.setItem("cart", JSON.stringify(state));
-      }
+
+      if (!productQuantity) return;
+
+      state.totalAmount -= productToRemove.product.price * productQuantity;
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+    updateQuantityOfItem: (
+      state,
+      action: PayloadAction<TUpdateQuantityOfItem>,
+    ) => {
+      const p = action.payload.data;
+
+      let productQuantity: number = action.payload.quantity;
+
+      const product = state.products.find(
+        ({ product }) => product._id === p._id,
+      );
+      if (!product) return;
+
+      product.quantity = productQuantity;
+      state.totalAmount = product.product.price * productQuantity;
+      localStorage.setItem("cart", JSON.stringify(state));
     },
     resetCart: (state) => {
       state.products = [];
@@ -107,6 +136,7 @@ export const {
   resetCart,
   saveShippingAddress,
   savePaymentMethod,
+  updateQuantityOfItem,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
