5b2195b3eba83213087ef13fe952030315b626e1
Sushant Sharma <sushant.wurk@gmail.com>
2024-12-11 18:44:28 +0530

    Migrate admin/productList

--- web/src/pages/admin/ProductsList.tsx
+++ web/src/pages/admin/ProductsList.tsx
@@ -1,4 +1,3 @@
-import { Table, Button, Row, Col, Alert } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -9,7 +8,18 @@ import {
 } from "../../features/products/slice";
 
 import Loader from "../../components/Loader";
-import Paginate from "../../components/Paginate";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Icon } from "@iconify/react";
+import Paginate from "@/components/Paginate";
+// import Paginate from "../../components/Paginate";
 
 const ProductList = () => {
   const { pageNum, keyword } = useParams();
@@ -60,69 +70,65 @@ const ProductList = () => {
 
   return (
     <>
-      <Row className="align-items-center">
-        <Col>
-          <h1>Products</h1>
-        </Col>
-        <Col className="text-end">
-          <Button
-            className="my-3"
-            onClick={createProductHandler}
-            disabled={loadingCreateProductMutation}
-          >
-            Create Product
-          </Button>
-        </Col>
-      </Row>
-
+      <div className="flex items-center w-full justify-between">
+        <h1 className="text-center text-sm italic">Products</h1>
+        <Button
+          className="my-3 place-self-end"
+          size="sm"
+          onClick={createProductHandler}
+          disabled={loadingCreateProductMutation}
+        >
+          Create
+        </Button>
+      </div>
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Alert variant="danger">{JSON.stringify(error, null, 2)}</Alert>
+        <pre>{JSON.stringify(error, null, 2)}</pre>
       ) : (
         <>
-          <Table striped bordered hover responsive className="table-sm">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>PRICE</th>
-                <th>CATEGORY</th>
-                <th>BRAND</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>NAME</TableHead>
+                <TableHead>PRICE</TableHead>
+                <TableHead>CATEGORY</TableHead>
+                <TableHead>BRAND</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
               {data.products.map((product) => (
-                <tr key={product._id}>
-                  <td>{product._id}</td>
-                  <td>{product.name}</td>
-                  <td>${product.price}</td>
-                  <td>{product.category}</td>
-                  <td>{product.brand}</td>
-                  <td>
+                <TableRow key={product._id}>
+                  <TableCell>{product._id}</TableCell>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>${product.price}</TableCell>
+                  <TableCell>{product.category}</TableCell>
+                  <TableCell>{product.brand}</TableCell>
+                  <TableCell className="lg:flex space-y-2 lg:space-y-0">
                     <Link
                       to={`/admin/product/${product._id}/edit`}
-                      className="btn btn-info btn-sm me-4"
+                      className="underline"
                     >
                       Edit
                     </Link>
                     <Button
-                      variant="danger"
-                      className="btn-sm"
+                      size="sm"
+                      variant="destructive"
                       onClick={() => deleteProductHandler(product._id)}
                       disabled={loadingDeleteMutation}
                     >
-                      delete
+                      <Icon icon="ic:baseline-delete" width="24" height="24" />
                     </Button>
-                  </td>
-                </tr>
+                  </TableCell>
+                </TableRow>
               ))}
-            </tbody>
+            </TableBody>
           </Table>
-          <Paginate pages={data.pages} page={data.page} isAdmin />
         </>
       )}
+      <Paginate pages={data.pages} page={data.page} isAdmin />
     </>
   );
 };
