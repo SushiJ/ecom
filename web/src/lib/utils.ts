7c3df4092dee0870cd6b1aea1c5f23b49c9ae851
Sushant Sharma <sushant.wurk@gmail.com>
2024-12-11 21:49:48 +0530

    useDelay hook

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+type ms = number;
+export function useDelay(time: ms) {
+  const [delay, setDelay] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setDelay(false), time);
+  }, []);
+  return delay;
 }
