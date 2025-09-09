06cab0672b9b546786274c137b3eedf1a97ec784
Sushant Sharma <sushant.wurk@gmail.com>
2025-09-09 22:44:20 +0530

    Chore. biome formating

--- sv/seed.ts
+++ sv/seed.ts
@@ -8,55 +8,55 @@ const uri = "mongodb://mongo:mongo@localhost:27017/";
 const client = new MongoClient(uri);
 
 async function seedDB() {
-  // Connection URL
-  await client.connect().catch((e) => console.error(e));
-  console.log("Connected correctly to server");
+	// Connection URL
+	await client.connect().catch((e) => console.error(e));
+	console.log("Connected correctly to server");
 
-  const userCollection = client.db("ecom").collection("users");
-  const productCollection = client.db("ecom").collection("products");
+	const userCollection = client.db("ecom").collection("users");
+	const productCollection = client.db("ecom").collection("products");
 
-  // The drop() command destroys all data from a collection.
-  // Make sure you run it against proper database and collection.
-  userCollection.drop();
-  productCollection.drop();
+	// The drop() command destroys all data from a collection.
+	// Make sure you run it against proper database and collection.
+	userCollection.drop();
+	productCollection.drop();
 
-  const userArr: Array<User> = [];
-  const product: Array<Product> = [];
+	const userArr: Array<User> = [];
+	const product: Array<Product> = [];
 
-  for (const u of users) {
-    const user: User = {
-      name: u.name,
-      password: u.password,
-      email: u.email,
-      role: u.role,
-    };
-    userArr.push(user);
-  }
+	for (const u of users) {
+		const user: User = {
+			name: u.name,
+			password: u.password,
+			email: u.email,
+			role: u.role,
+		};
+		userArr.push(user);
+	}
 
-  for (const p of products) {
-    const pro: Product = {
-      name: p.name,
-      price: p.price,
-      brand: p.brand,
-      image: p.image,
-      rating: p.rating,
-      category: p.category,
-      numReviews: p.numReviews,
-      description: p.description,
-      countInStock: p.countInStock,
-    };
-    product.push(pro);
-  }
+	for (const p of products) {
+		const pro: Product = {
+			name: p.name,
+			price: p.price,
+			brand: p.brand,
+			image: p.image,
+			rating: p.rating,
+			category: p.category,
+			numReviews: p.numReviews,
+			description: p.description,
+			countInStock: p.countInStock,
+		};
+		product.push(pro);
+	}
 
-  await userCollection.insertMany(userArr).catch((e) => console.error(e));
-  await userCollection.findOne().then(console.log);
-  await productCollection.insertMany(product).catch((e) => console.log(e));
-  await productCollection.findOne().then(console.log);
+	await userCollection.insertMany(userArr).catch((e) => console.error(e));
+	await userCollection.findOne().then(console.log);
+	await productCollection.insertMany(product).catch((e) => console.log(e));
+	await productCollection.findOne().then(console.log);
 
-  return "Database seeded";
+	return "Database seeded";
 }
 
 seedDB()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+	.then(console.log)
+	.catch(console.error)
+	.finally(() => client.close());
