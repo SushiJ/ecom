4a5eb36fa91af987ff615dc8bde10931bbc37434
Sushant Sharma <sushant.wurk@gmail.com>
2025-09-19 00:19:31 +0530

    Added more tests for products route

--- sv/tests/products.spec.ts
+++ sv/tests/products.spec.ts
@@ -138,34 +138,194 @@ test("DELETE /products/:id should return 200 user.role is admin", async () => {
 	expect(res.status).toBe(200);
 });
 
-// fastify.post(
-// 	"/",
-// 	{
-// 		onRequest: protect,
-// 		preHandler: isAdmin,
-// 	},
-// 	product.createProducts,
-// );
+test("POST /products should return 403 user.role is not admin", async () => {
+	const password = "123456";
+	const user = await database.setupUser("user");
+	const userLoginRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
 
-// fastify.put(
-// 	"/:id",
-// 	{
-// 		schema: {
-// 			params: mongoDBIdSchema,
-// 		},
-// 		onRequest: protect,
-// 		preHandler: isAdmin,
-// 	},
-// 	product.updateProduct,
-// );
+	expect(userLoginRes.status).toBe(200);
+	expect(userLoginRes.headers["set-cookie"]).toBeDefined();
 
-// fastify.post(
-// 	"/reviews/:id",
-// 	{
-// 		schema: {
-// 			params: mongoDBIdSchema,
-// 		},
-// 		onRequest: protect,
-// 	},
-// 	product.createProductReview,
-// );
+	const cookie = extractJwtFromSetCookie(userLoginRes.headers["set-cookie"]);
+
+	const res = await request
+		.post("/products")
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(403);
+	expect(res.body.message).toBe("Not authorized");
+});
+test("POST /products should return 201 & the product that was just created when user.role is admin", async () => {
+	const password = "123456";
+	const user = await database.setupUser("admin");
+	const adminUserRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(adminUserRes.status).toBe(200);
+	expect(adminUserRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(adminUserRes.headers["set-cookie"]);
+
+	const res = await request
+		.post("/products")
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(201);
+	expect(res.body.name).toBe("Sample name");
+});
+
+// May be not test the auth middlewares here since I have already tested them with post routes
+test("PUT /products/:id should return 400 when ID is not mongo db _id", async () => {
+	const password = "123456";
+	const user = await database.setupUser("admin");
+	const adminUserRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(adminUserRes.status).toBe(200);
+	expect(adminUserRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(adminUserRes.headers["set-cookie"]);
+
+	let id = "1";
+	const res = await request
+		.put(`/products/${id}`)
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(400);
+	expect(res.body).toHaveProperty("message");
+});
+test("PUT /products/:id should return 404 when ID doesn't match with any product", async () => {
+	const password = "123456";
+	const user = await database.setupUser("admin");
+	const adminUserRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(adminUserRes.status).toBe(200);
+	expect(adminUserRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(adminUserRes.headers["set-cookie"]);
+
+	const id = "68c17ccb1d2f253878dcc405";
+	const res = await request
+		.put(`/products/${id}`)
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(404);
+	expect(res.body).toHaveProperty("message");
+});
+test("PUT /products/:id should return 200 when product is successfully updated", async () => {
+	const password = "123456";
+	const user = await database.setupUser("admin");
+	const adminUserRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(adminUserRes.status).toBe(200);
+	expect(adminUserRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(adminUserRes.headers["set-cookie"]);
+
+	const { body } = await request
+		.post("/products")
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	const res = await request
+		.put(`/products/${body._id}`)
+		.send({
+			name: "Testing",
+			price: 1337,
+			description: "Testing the updation",
+			image: "test",
+			brand: "Tesvidia",
+			category: "electronics",
+			countInStock: 4,
+		})
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(200);
+	expect(res.body.name).toBe("Testing");
+});
+// Skipping the mongoDB _id check since it has already been tested different test using same schema
+test("POST /products/reviews/:id should return 404 when id doesn't match anything", async () => {
+	const password = "123456";
+	const user = await database.setupUser("user");
+	const userLoginRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(userLoginRes.status).toBe(200);
+	expect(userLoginRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(userLoginRes.headers["set-cookie"]);
+
+	const id = "68c17ccb1d2f253878dcc405";
+	const res = await request
+		.post(`/products/reviews/${id}`)
+		.send({
+			rating: 4,
+			comment: "Testing 123",
+		})
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(404);
+	expect(res.body).toHaveProperty("message");
+	expect(res.body.message).toBe("Product not found");
+});
+test("POST /products/reviews/:id should return 400 when body doesn't match", async () => {
+	const password = "123456";
+	const user = await database.setupUser("user");
+	const userLoginRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(userLoginRes.status).toBe(200);
+	expect(userLoginRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(userLoginRes.headers["set-cookie"]);
+
+	const id = "68c17ccb1d2f253878dcc405";
+	const res = await request
+		.post(`/products/reviews/${id}`)
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(400);
+	expect(res.body).toHaveProperty("message");
+});
+test("POST /products/reviews/:id should return 201 when success", async () => {
+	const password = "123456";
+	const user = await database.setupUser("user");
+	const userLoginRes = await request.post("/users/login").send({
+		email: user.email,
+		password,
+	});
+
+	expect(userLoginRes.status).toBe(200);
+	expect(userLoginRes.headers["set-cookie"]).toBeDefined();
+
+	const cookie = extractJwtFromSetCookie(userLoginRes.headers["set-cookie"]);
+
+	const product = await database.setupProduct();
+	const res = await request
+		.post(`/products/reviews/${product._id}`)
+		.send({
+			rating: 4,
+			comment: "Testing 123",
+		})
+		.set("Cookie", [`citrus=${cookie}`]);
+
+	expect(res.status).toBe(201);
+	expect(res.body.name).toBe(product.name);
+	expect(res.body.reviews.length).toBeGreaterThan(0);
+});
