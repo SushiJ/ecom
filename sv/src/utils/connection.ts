1e14b683dbdbf9f68885683d14d7a1cba7490832
Sushant Sharma <sushant.wurk@gmail.com>
2025-09-25 19:27:06 +0530

    chore. docker stuff

--- sv/src/utils/connection.ts
+++ sv/src/utils/connection.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 // mongodb://mongo:mongo@mongo:27017/ecom?authSource=admin
-// const MONGO_URL = "mongodb://mongo:27017/";
+const MONGO_URL = "mongodb://mongo:27017/";
 
-const uri = "mongodb://mongo:mongo@localhost:27017/";
+// const uri = "mongodb://mongo:mongo@localhost:27017/";
 
 export async function connect() {
-	return mongoose.connect(uri, {
+	return mongoose.connect(MONGO_URL, {
 		dbName: "ecom",
 		pass: "mongo",
 		user: "mongo",
@@ -14,7 +14,7 @@ export async function connect() {
 }
 
 export async function connectTestDb() {
-	return mongoose.connect(uri, {
+	return mongoose.connect(MONGO_URL, {
 		dbName: "test_db",
 		pass: "mongo",
 		user: "mongo",
