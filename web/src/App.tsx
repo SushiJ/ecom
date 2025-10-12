b3ebbb84e117227d8efa8c407cad3bca1081b835
Sushant Sharma <sushant.wurk@gmail.com>
2025-10-12 19:13:12 +0530

    feat. theme toggle

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,18 +5,21 @@ import "./index.css";
 
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
-	return (
-		<div className="container mx-auto px-6 h-full">
-			<Header />
-			<main className="py-3 flex-grow overflow-y-auto h-[90%] overflow-x-hidden">
-				<Outlet />
-			</main>
-			<ToastContainer theme="dark" position="bottom-right" autoClose={1000} />
-			<Footer />
-		</div>
-	);
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="ui-theme">
+      <div className="container mx-auto px-6 h-full">
+        <Header />
+        <main className="py-3 flex-grow overflow-y-auto h-[90%] overflow-x-hidden">
+          <Outlet />
+        </main>
+        <ToastContainer theme="dark" position="bottom-right" autoClose={1000} />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
